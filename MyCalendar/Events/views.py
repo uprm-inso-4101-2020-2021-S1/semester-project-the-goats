136b4843f8a2a351a2153ba2f69b2b5377a68a09
jorgeortiz41 <jorge.ortiz41@upr.edu>
2020-10-23 18:54:20 -0400

    fixed calendar view function to display calendar

--- MyCalendar/Events/views.py
+++ MyCalendar/Events/views.py
@@ -8,4 +8,4 @@ def CalendarView(request):
 
     cal = HTMLCalendar().formatmonth(2020,10,withyear=True).replace('<td ', '<td  width="150" height="150"')
 
-    return render(request, 'calendar.html', {'cal': cal})
+    return render(request, 'calendar.html', {'cal': mark_safe(cal)})
